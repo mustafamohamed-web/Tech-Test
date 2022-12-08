8b962ea0f6902ff6e0ec416a3192923ad631f358
mustafa mohammed <mustafamohammed@mustafas-MacBook-Pro.local>
2022-12-08 20:05:17 +0000

    form setup

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Form.css";
 
 const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [company, setCompany] = useState("");
+  const [message, setMessage] = useState("");
   return (
     <form className="form">
       <h1>Contact Form</h1>
 
-      <label htmlFor="">Name</label>
-      <input placeholder="name" />
+      <label htmlFor=""></label>
+      <input
+        placeholder="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
-      <label htmlFor="">Email</label>
-      <input placeholder="email" />
+      <label htmlFor=""></label>
+      <input
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
 
-      <label htmlFor="">Company</label>
-      <input placeholder="name" />
+      <label htmlFor=""></label>
+      <input
+        placeholder="company"
+        value={company}
+        onChange={(e) => setCompany(e.target.value)}
+      />
 
-      <label htmlFor="">Enqiry</label>
-      <input placeholder="name" />
+      <label htmlFor=""></label>
+      <textarea
+        placeholder="message"
+        onChange={(e) => setMessage(e.target.value)}
+      />
     </form>
   );
 };
